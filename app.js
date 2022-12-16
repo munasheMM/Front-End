1d25f78681d171f9e8e0da9ae893dc806f8ddd41
Munashe <shepherdjnrmm@gmail.com>
2022-12-17 00:08:31 +1030

    crash-course-javascript-v1

---
+++ app.js
@@ -0,0 +1,37 @@
+// string examples //
+
+    //console.log("hello world")
+    //console.log("hello, " + "Munahse")
+
+// Index Example //
+    //console.log("Hello World"[10])
+
+// Using .length prop //
+    //console.log("hello World".length)
+
+
+// Variable Examples //
+    //let fullName = "Munashe"
+
+    //let isRain = true
+    //let temp = 20
+    //let plant = "Earth"
+
+    //temp = temp + 2
+    //console.log(temp)
+
+//example Question 1:
+/**
+ * Converting Tempreature
+ * 
+ * Celsius-to-Fahrenheit formula:
+ * 
+ * F = C x 1.8 + 32
+ */
+
+    //let celsius = 10;
+    //let fahrenheit = celsius * 1.8 + 32
+
+    //console.log(fahrenheit);  //Code is correct//
+
+// Equality example //
