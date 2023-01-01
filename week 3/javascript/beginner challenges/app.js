19592c962b5b82fa8283173afe749aa858a7c5e2
Munashe <shepherdjnrmm@gmail.com>
2023-01-01 23:06:05 +1030

    Beginner Challenges

---
+++ week 3/javascript/beginner challenges/app.js
@@ -0,0 +1,48 @@
+// Question 1: Return the Sum of Two Numbers
+// create a function that takes two arguments as functions and returns their sum.
+/**
+ * function addition(num1, num2) {
+    return num1 + num2  
+}
+
+console.log(addition(3, 5));
+ * 
+ */
+
+// Question 2: Convert Hours into Seconds 
+// create a function that converts hours into seconds 
+/**
+ * function hoursIntoSeconds(hour) {
+    let seconds = hour * 60 * 60;
+    return seconds;
+}
+
+console.log(hoursIntoSeconds(2))
+ * 
+ */
+
+// Question 3: Calculate the perimeter of a Rectangle 
+/**
+ * 
+ * function calcPerimeter(width, length) {
+    let perimeter = (width + length) * 2
+    return perimeter
+}
+
+console.log(calcPerimeter(6,7))
+ */
+
+// Question 4: Calculate the Area of a Triangle
+// write a function that takes the base and height of a triangle and return its area
+/**
+ * function calcArea(base, height) {
+    let area = 0.5 * base * height
+    return area
+}
+
+console.log(calcArea(10,10))
+ *  
+ */
+
+// Question 5: Extend a String
+// write a function accepts a string and adds 'Frontend' onto the end of it 
