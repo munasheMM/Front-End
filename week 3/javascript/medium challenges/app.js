235019527e6776e98103f4337931019205cbffc2
Munashe <shepherdjnrmm@gmail.com>
2023-01-06 14:34:19 +1030

    Medium Challenges-v1

---
+++ week 3/javascript/medium challenges/app.js
@@ -0,0 +1,13 @@
+// Q1: Falsy or Truthy
+// Given two values, return the first one if it is falsy, otherwise return the second one.
+/**
+ * function filterOutFalsy(val1,val2) {
+    return (!val1) ? val1 : val2
+}
+
+console.log(filterOutFalsy(0, 500))
+ * 
+ */
+
+// Q2: Return the length of any given array
+// Given an array, return its length
