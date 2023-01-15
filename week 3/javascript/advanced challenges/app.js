3650d1d0163e22dbf0e59c80600ce420c99f3a05
Munashe <shepherdjnrmm@gmail.com>
2023-01-15 22:51:11 +1030

    Advance Challenges-v1

---
+++ week 3/javascript/advanced challenges/app.js
@@ -0,0 +1,9 @@
+//Q1: Show rating
+//Given a rating, display a star (*) for each
+//full rating and a full-stop(.) for each half rating
+
+function showRating(rating) {
+
+}
+
+console.log(showRating(4.5))
